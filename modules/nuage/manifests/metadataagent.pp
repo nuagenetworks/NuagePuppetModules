a43f01040be42b1cf7de27d2d8e488b72f2ec76e
Lokesh Jain <lokesh.jain@gmail.com>
2017-09-12 13:41:33 -0400

    Updated metadata with extra params for Newton and later

--- modules/nuage/manifests/metadataagent.pp
+++ modules/nuage/manifests/metadataagent.pp
@@ -4,47 +4,62 @@
 #
 # === Parameters
 #
-# [*nuage_metadata_port*]
-#  TCP Port to listen for metadata server requests 
+# [*metadata_port*]
+#  TCP Port to listen for metadata server requests
 # (metadata_port in neutron.conf)
-# 
-# [*nuage_nova_metadata_ip*]
-#  IP address used by Nova metadata server 
+#
+# [*nova_metadata_ip*]
+#  IP address used by Nova metadata server
 # (nova_metadata_ip in neutron.conf)
 #
-# [*nuage_nova_metadata_port*]
-#  TCP Port used by Nova metadata server 
+# [*nova_metadata_port*]
+#  TCP Port used by Nova metadata server
 #  (metadata_listen_port in nova.conf or nova_metadata_port in neutron.conf)
 #
-# [*shared_secret*]
-#  Shared secret to sign the instance-id request 
-# (neutron_metadata_proxy_shared_secret in nova.conf or 
+# [*metadata_secret*]
+#  Shared secret to sign the instance-id request
+# (neutron_metadata_proxy_shared_secret in nova.conf or
 #   metadata_proxy_shared_secret in neutron.conf)
 #
-# [*nuage_nova_client_version*]
+# [*nova_client_version*]
 #  Defaults to '2'
 #
-# [*nuage_nova_os_username*]
+# [*nova_os_username*]
 #  Defaults to 'admin'
 #
-# [*nuage_nova_os_password*]
+# [*nova_os_password*]
 #  Defaults to 'admin'
 #
-# [*nuage_nova_os_tenant_name*]
+# [*nova_os_tenant_name*]
 #  Defaults to "demo"
 #
-# [*nuage_nova_auth_url*]
-#  Defaults to http://<nova_metadata_ip>:5000/v2.0
+# [*nova_auth_url*]
+#  Defaults to http://<nova_auth_ip>:5000/v<identity_url_version>
 #
-# [*nuage_metadata_agent_start_with_ovs*]
-# Set to True if nuage-metadata-agent needs to be started with 
+# [*metadata_agent_start_with_ovs*]
+# Set to True if nuage-metadata-agent needs to be started with
 # nuage-openvswitch-switch
 #
-# [*nuage_nova_api_endpoint_type*]
+# [*nova_api_endpoint_type*]
 #  One of publicURL, internalURL, adminURL
 #
-# [*nuage_nova_region_name*]
+# [*nova_region_name*]
 #  Based on keystone endpoint-list
+#
+# [*nova_project_name*]
+#  Name of the default nova project (for eg. services)
+#
+# [*nova_user_domain_name*]
+#  User domain name that nova service uses (pick this from nova.conf)
+#
+# [*nova_project_domain_name*]
+#  User project name that nova service uses (pick this from nova.conf)
+#
+# [*identity_url_version*]
+#  Url version of keystone identity. Defaults to 3
+#
+# [*nova_os_keystone_username*]
+#  Keystone username used by nova
 #
 # [*package_ensure*]
 #  To ensure that the nuage-metadata-agent package is available
@@ -62,15 +77,16 @@ class nuage::metadataagent(
   $metadata_agent_start_with_ovs = 'true',
   $nova_api_endpoint_type = 'publicURL',
   $nova_region_name = 'RegionOne',
+  $nova_project_name = 'service',
+  $nova_user_domain_name = 'default',
+  $nova_project_domain_name = 'default',
+  $identity_url_version = '3',
+  $nova_os_keystone_username = 'nova',
   $package_ensure = present,
 ) {
 
-  if $::neutron::core_plugin != 'neutron.plugins.nuage.plugin.NuagePlugin' {
-    fail('Nuage plugin should be the core_plugin to use nuage-metadata-agent')
-  }
-  
   include ::nuage::params
-  
+
   package { $::nuage::params::python_novaclient:
     ensure => present,
   }
